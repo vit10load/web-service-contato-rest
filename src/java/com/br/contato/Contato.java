470cfb0a41ef49ae9119c147c41481722d1b8611
vit10load <vitoro580@gmail.com>
2020-06-17 10:55:04 -0400

    teste do web service de contato

---
+++ src/java/com/br/contato/Contato.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.contato;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author vitcl
+ */
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Contato {
+    
+    private String nome;
+    private String email;
+
+    public Contato() {
+    }
+    
+    public Contato(String nome, String email){
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+}
---
+++ src/java/com/br/contato/ContatoResource.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.br.contato;
+
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author vitcl
+ */
+@Path("contato")
+public class ContatoResource {
+
+
+    /**
+     * Creates a new instance of ContatoResource
+     */
+    public ContatoResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of com.br.contato.ContatoResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String retornaNomeContato() {
+        //TODO return proper representation object
+        return "<nome>Vitor Oliveira da Silva</nome>";
+    }
+    
+    // Exemplo de objeto a ser trafegado no SERVICE
+    /*
+        <contato>
+            <nome></nome>
+            <email></email>
+        </contato>
+    */
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public void inserirContato(Contato contato){
+        System.out.println(contato.getEmail());
+        System.out.println(contato.getNome());
+    }
+
+    /**
+     * PUT method for updating or creating an instance of ContatoResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
